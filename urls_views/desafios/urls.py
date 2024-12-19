0a5d13f22e69221c8d06bca22d4e2365e537c282
miguel977 <miguel645@gmail.com>
2024-12-19 12:29:34 -0300

    refaturando as funções

--- urls_views/desafios/urls.py
+++ urls_views/desafios/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from.views import desafio_semana, desafio_semana_numero
 urlpatterns = [
     path('<int:dia>', desafio_semana_numero),
-    path('<str:dia>', desafio_semana),
+    path('<str:dia>', desafio_semana, name='desafio_semana'),
 ]
